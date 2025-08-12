cb5bcc74582bd36c227077202e6127ce6fc20988
dylan3141592 <dylan314.dev@gmail.com>
2025-08-12 19:31:37 +0900

    cp_v1.2

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+
 import { BrowserRouter} from 'react-router-dom'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
+      
+      <Footer />
     </BrowserRouter>
 
   )
